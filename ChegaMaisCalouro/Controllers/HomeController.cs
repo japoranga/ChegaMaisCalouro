f4312acc6e183201a7e00308785a067fd63495f8
japoranga <otavio.tomaz@hotmail.com>
2017-08-04 07:44:07 -0300

    mens

---
+++ ChegaMaisCalouro/Controllers/HomeController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChegaMaisCalouro.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+
+        public ActionResult Guia()
+        {
+            return View();
+        }
+
+        public ActionResult Sobre()
+        {
+            return View();
+        }
+
+        public ActionResult Faculdades()
+        {
+            return View();
+        }
+
+        public ActionResult Vestibulares()
+        {
+            return View();
+        }
+
+        public ActionResult Português()
+        {
+            return View();
+        }
+
+        public ActionResult Gramática()
+        {
+            return View();
+        }
+
+        public ActionResult Matemática()
+        {
+            return View();
+        }
+
+        public ActionResult Biologia()
+        {
+            return View();
+        }
+
+        public ActionResult História()
+        {
+            return View();
+        }
+
+        public ActionResult Geografia()
+        {
+            return View();
+        }
+
+        public ActionResult Física()
+        {
+            return View();
+        }
+
+        public ActionResult Sociologia()
+        {
+            return View();
+        }
+
+        public ActionResult Filosofia()
+        {
+            return View();
+        }
+
+        public ActionResult Química()
+        {
+            return View();
+        }
+
+        public ActionResult Ingles()
+        {
+            return View();
+        }
+
+        public ActionResult Espanhol()
+        {
+            return View();
+        }
+       
+    }
+}
